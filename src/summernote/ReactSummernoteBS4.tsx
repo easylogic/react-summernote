9137d657c90f9f8bbdd3434e82c0489a4732bd62
easylogic <cyberuls@gmail.com>
2020-03-17 22:31:48 +0900

    support createSummernotePlugin for bs4

--- src/summernote/ReactSummernoteBS4.tsx
+++ src/summernote/ReactSummernoteBS4.tsx
@@ -1,15 +1,76 @@
 import React from 'react';
-import jquery from 'jquery';
-window.jQuery = jquery;
-window.$ = jquery;
+import * as ReactDOM from 'react-dom';
+import $ from 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'summernote/dist/summernote-bs4';
 import 'summernote/dist/summernote-bs4.css';
 import Summernote from 'src/component/Summernote';
-import { SummernoteProps } from 'src/index';
+import { SummernoteProps, SummernoteContext, SummernoteCustomButtonProps, SummernotePluginInterface } from 'src/index';
 
+
+interface ButtonProps {
+    container?: string;
+    tooltip?: string;
+    contents?: string;
+    click?: () => void; 
+}
+
+interface Attribute {
+    context?: SummernoteContext;
+}
+
+export function createSummernoteButton (opt: SummernoteCustomButtonProps): any {
+    return (context: SummernoteContext) => {
+        let buttonProps: ButtonProps = {
+            container: opt.container || 'body',
+            tooltip: opt.tooltip || 'sample',
+        }
+
+        if (opt.title && !opt.element ) {
+            buttonProps.contents = opt.title;
+            buttonProps.click = () => {
+                opt?.onClick(context);
+            }
+            return context.ui.button(buttonProps).render();
+        } else {
+            const button = context.ui.button(buttonProps)
+            const el = button.render();   // return button as jquery object
+    
+            if (opt?.element) {
+                const props = {context, ...opt.props};
+                ReactDOM.render(React.createElement<Attribute>(opt.element, props), el[0])
+            }
+    
+        
+            return el;
+        }
+    }
+}
+
+export class SummernotePlugin {
+    context: SummernoteContext;
+    $: JQueryStatic;
+    constructor(context: SummernoteContext, $: JQueryStatic) {
+        this.context = context;
+        this.$ = $; 
+    }
+}
+
+export function createSummernotePlugin (name: string, PluginClass: SummernotePluginInterface) {
+    const jQuery = ($ as any)
+
+    if (jQuery.summernote.plugins[name]) {
+        console.warn(`${name} plugin is already exists.`)
+    }
+
+    jQuery.extend(jQuery.summernote.plugins, {
+        [name]: (context:SummernoteContext) => {
+            return new PluginClass(context, jQuery)
+        } 
+    })
+}
 
 function ReactSummernoteBS4({children, ...props}: SummernoteProps) {
     return (
